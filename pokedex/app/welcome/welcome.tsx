9f2c353def2cb40752bfaf4381764695b99783e8
Ralyana Kethleen <rallyanaket@gmail.com>
2026-03-26 10:20:10 -0300

    feat: :sparkles: LI E TENTEI FAZER
    FAZENDO SITE

---
+++ pokedex/app/welcome/welcome.tsx
@@ -0,0 +1,15 @@
+
+export function Welcome() {
+
+//border 2px solid black, black
+
+  return (
+    <main className="flex flex-col intems-center min-h-screen">
+      <div className="Topo-Colorido"></div>
+      <h1 className="text-center">Pokédex</h1>
+      <button className="border-2 bg-red-600 border-red-60 p-2">Home  Pokemon  Tipos </button>
+     <div className="red-sky-500/20"></div>
+    </main>
+  );
+}
+
